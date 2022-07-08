03d59fa1fd13bb490879fd265a2d7cb73384a2af
Kyler Larsen <63459392+Aopsmath99@users.noreply.github.com>
2022-07-08 09:47:53 -0400

    First try with transformer
    Found some code online, tried to get it to compile, made a few changes and I think this code at least runs now. Will try to get better at this stuff soon.

---
+++ SampleTransformer.py
@@ -0,0 +1,117 @@
+########################################################################################################################################################################
+from keras.datasets import cifar10
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+print(f"x_train: {x_train.shape} - y_train: {y_train.shape}")
+print(f"x_test: {x_test.shape} - y_test: {y_test.shape}")
+learning_rate = 0.001
+weight_decay = 0.0001
+batch_size = 256
+num_epochs = 10
+image_size = 72
+patch_size = 6  
+num_patches = (image_size // patch_size) ** 2
+projection_dim = 64
+num_heads = 4
+transformer_units = [projection_dim * 2, projection_dim,] 
+transformer_layers = 8
+mlp_head_units = [2048, 1024]
+########################################################################################################################################################################
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+import tensorflow_addons as tfa
+import matplotlib.pyplot as plt
+data_augmentation = keras.Sequential(
+    [
+        layers.Normalization(),
+        layers.Resizing(72, 72),
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(factor=0.02),
+        layers.RandomZoom(
+            height_factor=0.2, width_factor=0.2
+        ),
+    ],
+    name="data_augmentation",
+)
+data_augmentation.layers[0].adapt(x_train)
+
+def mlp(x, hidden_units, dropout_rate):
+    for units in hidden_units:
+        x = layers.Dense(units, activation=tf.nn.gelu)(x)
+        x = layers.Dropout(dropout_rate)(x)
+    return x
+########################################################################################################################################################################
+class Patches(layers.Layer):
+    def __init__(self, patch_size):
+        super(Patches, self).__init__()
+        self.patch_size = patch_size
+ 
+    def call(self, images):
+        batch_size = tf.shape(images)[0]
+        patches = tf.image.extract_patches(
+            images=images,
+            sizes=[1, self.patch_size, self.patch_size, 1],
+            strides=[1, self.patch_size, self.patch_size, 1],
+            rates=[1, 1, 1, 1],
+            padding="VALID",
+        )
+        patch_dims = patches.shape[-1]
+        patches = tf.reshape(patches, [batch_size, -1, patch_dims])
+        return patches
+########################################################################################################################################################################
+class PatchEncoder(layers.Layer):
+    def __init__(self, num_patches, projection_dim):
+        super(PatchEncoder, self).__init__()
+        self.num_patches = num_patches
+        self.projection = layers.Dense(units=projection_dim)
+        self.position_embedding = layers.Embedding(
+            input_dim=num_patches, output_dim=projection_dim
+        )
+ 
+    def call(self, patch):
+        positions = tf.range(start=0, limit=self.num_patches, delta=1)
+        encoded = self.projection(patch) + self.position_embedding(positions)
+        return encoded
+########################################################################################################################################################################
+def create_vit_classifier():
+    inputs = layers.Input(shape=(32, 32, 3))
+    augmented = data_augmentation(inputs)
+    patches = Patches(patch_size)(augmented)
+    encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)
+ 
+    for _ in range(transformer_layers):
+        x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+        attention_output = layers.MultiHeadAttention(
+            num_heads=num_heads, key_dim=projection_dim, dropout=0.1
+        )(x1, x1)
+        x2 = layers.Add()([attention_output, encoded_patches])
+        x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
+        x3 = mlp(x3, hidden_units=transformer_units, dropout_rate=0.1)
+        encoded_patches = layers.Add()([x3, x2])
+ 
+    representation = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+    representation = layers.Flatten()(representation)
+    representation = layers.Dropout(0.5)(representation)
+    features = mlp(representation, hidden_units=mlp_head_units, dropout_rate=0.5)
+    logits = layers.Dense(1000)(features)
+    model = keras.Model(inputs=inputs, outputs=logits)
+    return model
+########################################################################################################################################################################
+model = create_vit_classifier()
+optimizer = tfa.optimizers.AdamW(learning_rate=learning_rate, weight_decay=weight_decay)
+model.compile(
+    optimizer=optimizer,
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=[
+       keras.metrics.SparseCategoricalAccuracy(name="accuracy"),
+       keras.metrics.SparseTopKCategoricalAccuracy(5, name="top-5-accuracy"), ],)
+history = model.fit(
+    x=x_train,
+    y=y_train,
+    batch_size=batch_size,
+    epochs=num_epochs,
+    validation_split=0.1,)
+accuracy, top_5_accuracy = model.evaluate(x_test, y_test)
+print(f"Test accuracy: {round(accuracy * 100, 2)}%")
+print(f"Test top 5 accuracy: {round(top_5_accuracy * 100, 2)}%")
